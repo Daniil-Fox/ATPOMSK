d58ede2c6a082107a0828042afe4f9ecbeb6c98a
Artyushenko Daniil <foxones80@gmail.com>
2025-06-21 00:32:06 +0600

    fixed some

--- src/js/components/sliders.js
+++ src/js/components/sliders.js
@@ -1,6 +1,6 @@
 import { Swiper } from "swiper";
-import { Pagination } from "swiper/modules";
-Swiper.use([Pagination]);
+import { Pagination, Thumbs } from "swiper/modules";
+Swiper.use([Pagination, Thumbs]);
 
 new Swiper(".about__slider", {
   slidesPerView: 3,
@@ -17,5 +17,18 @@ new Swiper(".about__slider", {
     769: {
       sliderPerView: 3,
     },
+  },
+});
+
+const thumbsProd = new Swiper(".product__thumbs", {
+  slidesPerView: "auto",
+  spaceBetween: 10,
+});
+new Swiper(".product__slider", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  speed: 500,
+  thumbs: {
+    swiper: thumbsProd,
   },
 });
